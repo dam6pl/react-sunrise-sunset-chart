030c19646ff2f55fb9ce65c5d4c536ea05213dea
Damian Nowak <damiannowak@nurtureagency.com>
2019-08-07 07:33:41 +0200

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 const app = express();
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
